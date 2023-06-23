f82f85a85564a6ef6b1ef5c105be21f89e45a8f2
Ricardo Pereira <ricardo.pereira.developpeur@gmail.com>
2023-06-23 16:34:22 +0200

    first commit

---
+++ src/index_old.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import {typeDefs} from './typeDefs';
+import { resolvers } from './resolver';
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+  
+console.log(`🚀  Server ready at: ${url}`);
---
+++ src/resolver.ts
@@ -0,0 +1,7 @@
+import { books } from "./data";
+
+export const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
